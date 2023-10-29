76e1652478d73c818b79b5018b08573ae42ef81d
bmacnaughton <bmacnaughton@gmail.com>
2023-10-29 15:20:12 -0700

    add other require hooks

--- hook.cjs
+++ hook.cjs
@@ -1,8 +1,10 @@
 const Module = require("node:module");
 
 const originalRequire = Module.prototype.require;
+const originalCompile = Module.prototype._compile;
+const originalExtensions = Module._extensions['.js'];
 
-Module.prototype.require = (moduleId) => {
+Module.prototype.require = function(moduleId) {
   if (moduleId === "node:http") {
     return {
       fake: true,
@@ -10,5 +12,15 @@ Module.prototype.require = (moduleId) => {
     };
   }
 
-  return originalRequire(moduleId);
+  return originalRequire.call(this, moduleId);
+};
+
+Module.prototype._compile = function(code, filename) {
+  console.log("CJS -> _compile() called for", filename);
+  return originalCompile.call(this, code, filename);
+};
+
+Module._extensions['.js'] = function(module, filename) {
+  console.log("CJS -> _extensions['.js'] called for", filename);
+  return originalExtensions.call(this, module, filename);
 };
