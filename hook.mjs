98bd3a83b8e2231ba2651e0483d3887822d08497
Vladimir Grenaderov <kosh@vorlon.co>
2023-10-29 16:42:08 +0000

    test

--- hook.mjs
+++ hook.mjs
@@ -1,18 +1,45 @@
+import {readFile} from 'node:fs/promises';
+
+const modulesToPatch = ['node:http'] // for 'http' and other builtins without 'node:' we can just add 'node:' prefix before making decision
+const moduleMocks = {
+  "http": new URL('./http-mock.mjs', import.meta.url)
+}
+
 export async function resolve(specifier, context, nextResolve) {
   console.log("ESM HOOK -> RESOLVE", specifier);
+
+  const isMockFile = context.parentURL?.endsWith('fake=1') === true;
+  // console.log('isMockFile', isMockFile)
+  
+  if (!isMockFile && modulesToPatch.includes(specifier)) {
+    return {
+      url: `${specifier}?fake=1`,
+      format: 'module', // sure, we should check packege.json type for non-builtin modules, but it's an example, right?
+      shortCircuit: true,
+    }    
+  }
+
   return nextResolve(specifier, context);
 }
 
 export const load = async (url, context, nextLoad) => {
   console.log("ESM HOOK -> LOAD", url);
 
-  if (url === 'node:http') {
-    return {
-      source: 'export default {fake: true, source: "esm hook"}',
-      shortCircuit: true,
-      format: 'module'
+  const urlObject = new URL(url);
+
+  if (urlObject.searchParams.has('fake')) {
+    const mockUrl = moduleMocks[urlObject.pathname];
+    if (mockUrl !== undefined) {
+      const source = await readFile(mockUrl, 'utf8');
+
+      return {
+        source,
+        shortCircuit: true,
+        format: 'module'
+      }
+  
     }
   }
- 
+
   return nextLoad(url, context);
 };
---
+++ http-mock.mjs
@@ -0,0 +1,7 @@
+import {METHODS} from 'node:http';
+
+export default {
+    fake: true,
+    source: 'http esm mock',
+    partOfRealModule: METHODS
+};
