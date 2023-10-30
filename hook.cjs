7b1db3ac2ab7292fa9765693804840184a95fd56
bmacnaughton <bmacnaughton@gmail.com>
2023-10-30 12:58:24 -0700

    working with register(), not pre-20.6

--- hook.cjs
+++ hook.cjs
@@ -1,14 +1,16 @@
 const Module = require("node:module");
 
+const semver = require('semver');
+
 const originalRequire = Module.prototype.require;
 const originalCompile = Module.prototype._compile;
 const originalExtensions = Module._extensions['.js'];
 
 Module.prototype.require = function(moduleId) {
-  if (moduleId === "node:http") {
+  if (moduleId === 'node:http') {
     return {
       fake: true,
-      source: "cjs hook"
+      source: 'cjs hook'
     };
   }
 
@@ -16,11 +18,18 @@ Module.prototype.require = function(moduleId) {
 };
 
 Module.prototype._compile = function(code, filename) {
-  console.log("CJS -> _compile() called for", filename);
+  console.log('CJS -> _compile() called for', filename);
   return originalCompile.call(this, code, filename);
 };
 
 Module._extensions['.js'] = function(module, filename) {
-  console.log("CJS -> _extensions['.js'] called for", filename);
+  console.log('CJS -> _extensions[".js"] called for', filename);
   return originalExtensions.call(this, module, filename);
 };
+
+if (semver.gte(process.version, '20.6.0')) {
+  // this fails with no errors
+  //Module.register('./hook.mjs');
+} else {
+  //throw new Error('non-register() hooks not yet implemented');
+}
--- hook.mjs
+++ hook.mjs
@@ -5,18 +5,27 @@ const moduleMocks = {
   "http": new URL('./http-mock.mjs', import.meta.url)
 }
 
+//export function globalPreload() {
+//  console.log("ESM HOOK -> GLOBAL PRELOAD");
+//  return '';
+//}
+
+export async function initialize() {
+  console.log("ESM HOOK -> INIT");
+}
+
 export async function resolve(specifier, context, nextResolve) {
   console.log("ESM HOOK -> RESOLVE", specifier);
 
   const isMockFile = context.parentURL?.endsWith('fake=1') === true;
   // console.log('isMockFile', isMockFile)
-  
+
   if (!isMockFile && modulesToPatch.includes(specifier)) {
     return {
       url: `${specifier}?fake=1`,
       format: 'module', // sure, we should check packege.json type for non-builtin modules, but it's an example, right?
       shortCircuit: true,
-    }    
+    }
   }
 
   return nextResolve(specifier, context);
@@ -37,9 +46,19 @@ export const load = async (url, context, nextLoad) => {
         shortCircuit: true,
         format: 'module'
       }
-  
+
     }
   }
 
   return nextLoad(url, context);
 };
+
+function protectedNextResolve(nextResolve, specifier, context) {
+  if (context.parentURL) {
+    if (context.conditions.slice(-1)[0] === 'node-addons' || context.importAssertions || isLT16_12) {
+      return nextResolve(specifier, context);
+    }
+  }
+
+  return nextResolve(specifier);
+}
